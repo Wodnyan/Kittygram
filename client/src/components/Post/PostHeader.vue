54daa3072e9c2e64e58dd77d97b4c229c9b96e0f
Albert Gergo <gergoalbert.gta@gmail.com>
2021-01-19 18:25:38 +0200

    feat: post options overlay

--- client/src/components/Post/PostHeader.vue
+++ client/src/components/Post/PostHeader.vue
@@ -4,9 +4,45 @@
       <v-img alt="Avatar" :src="avatar" />
     </v-avatar>
     <h1 class="text-h6 font-weight-bold">{{ username }}</h1>
-    <v-btn fab x-small elevation="1" class="ml-auto transparent">
+    <v-btn
+      @click="overlay = !overlay"
+      fab
+      x-small
+      elevation="1"
+      class="ml-auto transparent"
+    >
       <v-icon dark>mdi-dots-horizontal</v-icon>
     </v-btn>
+    <v-overlay :value="overlay">
+      <v-card class="mx-auto">
+        <v-list class="list">
+          <v-list-item @click="report">
+            <v-list-item-content>
+              <v-list-item-title class="text-center">Report</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item @click="unfollow">
+            <v-list-item-content>
+              <v-list-item-title class="text-center"
+                >Unfollow</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item @click="copyLink">
+            <v-list-item-content>
+              <v-list-item-title class="text-center"
+                >Copy Link</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item @click="closeOverlay">
+            <v-list-item-content>
+              <v-list-item-title class="text-center">Close</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-overlay>
   </header>
 </template>
 
@@ -15,6 +51,28 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "PostAvatar",
+  data() {
+    return {
+      overlay: false,
+    };
+  },
+  methods: {
+    closeOverlay() {
+      this.overlay = false;
+    },
+    copyLink() {
+      console.log("copied link");
+    },
+    report() {
+      console.log("reported");
+    },
+    follow() {
+      console.log("followed");
+    },
+    unfollow() {
+      console.log("unfollowed");
+    },
+  },
   props: {
     username: String,
     avatar: String,
@@ -22,4 +80,9 @@ export default Vue.extend({
 });
 </script>
 
-<style lang="scsss" scoped></style>
+<style lang="scss" scoped>
+.list {
+  max-width: 500px;
+  width: 100vw;
+}
+</style>
