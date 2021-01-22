c1afc07337d38ea97492f4c7f4c745673e5445ff
Albert Gergo <gergoalbert.gta@gmail.com>
2021-01-22 20:54:40 +0200

    test: tests now run synchronously, users test

--- backend/src/api/auth/auth.test.ts
+++ backend/src/api/auth/auth.test.ts
@@ -3,12 +3,12 @@ import app from "../../app";
 import { connection } from "../../db";
 import { respondMessages } from "./auth.controllers";
 
-beforeEach(async () => {
-  await connection.migrate.rollback();
+beforeAll(async () => {
   await connection.migrate.latest();
 });
 
 afterAll(async () => {
+  await connection.migrate.rollback();
   await connection.destroy();
 });
 
@@ -47,29 +47,3 @@ describe("POST /api/v1/auth/sign-up", () => {
     expect(response.body.message).toBe(respondMessages.validationError);
   });
 });
-
-// const testLoginUserBadCredentials = {
-//   username: "foo",
-//   password: "i'm bad",
-// };
-
-// describe("POST /api/v1/auth/login", () => {
-//   it("should respond with a token", async () => {
-//     const response = await supertest(app)
-//       .post("/api/v1/auth/login")
-//       .send(testLoginUser)
-//       .set("Accept", "application/json")
-//       .expect("Content-Type", /json/)
-//       .expect(200);
-//     expect(response.body.message).toBe("what");
-//   });
-//   it("it should respond with 401 error and an error message", async () => {
-//     const response = await supertest(app)
-//       .post("/api/v1/auth/login")
-//       .send(testLoginUserBadCredentials)
-//       .set("Accept", "application/json")
-//       .expect("Content-Type", /json/)
-//       .expect(401);
-//     expect(response.body.message).toBe("Incorrect credentials");
-//   });
-// });
