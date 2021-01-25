c342f74668dbeb1ebf9b89d5c0fd5d0e8fea0ccb
Albert Gergo <gergoalbert.gta@gmail.com>
2021-01-25 21:48:45 +0200

    feat: respond with better looking post, starting working on likes

--- backend/src/api/api.ts
+++ backend/src/api/api.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import auth from "./auth/auth.routes";
 import users from "./users/users.routes";
 import posts from "./posts/posts.routes";
+import likes from "./likes/likes.routes";
 
 const router = Router();
 
 router.use("/auth", auth);
 router.use("/users", users);
 router.use("/posts", posts);
+router.use("/likes", likes);
 
 export const messages = {
   root: "Welcome to my API",
