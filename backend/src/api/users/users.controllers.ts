52d4c1b7c06ddd7fe513df56cf11b68d03634c32
Albert Gergo <gergoalbert.gta@gmail.com>
2021-01-27 20:26:10 +0200

    feat: route for requesting the posts of 1 user

--- backend/src/api/users/users.controllers.ts
+++ backend/src/api/users/users.controllers.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Request, Response } from "express";
+import FormatedPost from "../../lib/formated-post";
+import { allPostsQueryParams } from "../../validation-schemas/posts";
 import User from "../auth/auth.model";
+import Like from "../likes/likes.model";
+import Post from "../posts/posts.model";
 
 export const getUserInfo = async (
   req: Request,
@@ -11,6 +15,48 @@ export const getUserInfo = async (
     res.json({
       user,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPostsOfUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { userId } = req.params;
+  const { skip, limit } = req.query;
+  await allPostsQueryParams.validateAsync(
+    { userId, skip, limit },
+    {
+      abortEarly: false,
+    }
+  );
+  const posts = await Post.query()
+    .leftJoinRelated("poster")
+    .where({
+      user_id: userId,
+    })
+    .select([
+      "posts.id as id",
+      "posts.description as description",
+      "image",
+      "poster.username as poster",
+      "poster.email as posterEmail",
+      "poster.id as posterId",
+    ])
+    .offset(Number(skip))
+    .limit(Number(limit));
+  const likes = await Like.query()
+    .where({ user_id: Number(userId) || undefined })
+    .skipUndefined();
+  const formatedPosts = posts.map((post: any) => {
+    const isLiked = likes?.some((like) => like.post_id === post.id);
+    return new FormatedPost({ ...post, liked: isLiked });
+  });
+  res.json({ posts: formatedPosts });
+  try {
   } catch (error) {
     next(error);
   }
