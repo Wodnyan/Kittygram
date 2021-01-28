d23e183b5d0e9c7b1978a2c1743319edc2d53166
Albert Gergo <gergoalbert.gta@gmail.com>
2021-01-28 21:02:54 +0200

    refactor: removed the router-link comp

--- client/src/components/Nav/Nav.vue
+++ client/src/components/Nav/Nav.vue
@@ -5,51 +5,45 @@
 
     <v-spacer></v-spacer>
 
-    <router-link to="/">
-      <v-btn icon>
-        <v-icon>{{ setIcon("/", "mdi-home", "mdi-home-outline") }}</v-icon>
-      </v-btn>
-    </router-link>
+    <v-btn to="/" icon>
+      <v-icon>{{ setIcon("/", "mdi-home", "mdi-home-outline") }}</v-icon>
+    </v-btn>
 
-    <router-link to="/explore">
-      <v-btn icon>
-        <v-icon>{{
-          setIcon("/explore", "mdi-compass", "mdi-compass-outline")
-        }}</v-icon>
-      </v-btn>
-    </router-link>
+    <v-btn to="/explore" icon>
+      <v-icon>{{
+        setIcon("/explore", "mdi-compass", "mdi-compass-outline")
+      }}</v-icon>
+    </v-btn>
 
-    <router-link to="/liked">
-      <v-btn icon>
-        <v-icon>{{
-          setIcon("/liked", "mdi-heart", "mdi-heart-outline")
-        }}</v-icon>
-      </v-btn>
-    </router-link>
+    <v-btn to="/liked" icon>
+      <v-icon>{{ setIcon("/liked", "mdi-heart", "mdi-heart-outline") }}</v-icon>
+    </v-btn>
 
-    <router-link to="/new-post">
-      <v-btn icon>
-        <v-icon size="25">{{
-          setIcon("/new-post", "mdi-pencil-circle", "mdi-pencil-circle-outline")
-        }}</v-icon>
-      </v-btn>
-    </router-link>
+    <v-btn to="/new-post" icon>
+      <v-icon size="25">{{
+        setIcon("/new-post", "mdi-pencil-circle", "mdi-pencil-circle-outline")
+      }}</v-icon>
+    </v-btn>
 
-    <router-link to="/user">
-      <v-btn icon>
-        <v-avatar color="secondary" size="25"></v-avatar>
-      </v-btn>
-    </router-link>
+    <v-btn :to="userRoute()" icon>
+      <v-avatar color="secondary" size="25">
+      <v-img alt="Avatar" :src="$store.state.user.avatar" />
+      </v-avatar>
+    </v-btn>
   </v-app-bar>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 export default Vue.extend({
   methods: {
     setIcon(path: string, active: string, inActive: string) {
       return this.$router.currentRoute.path === path ? active : inActive;
+    },
+    userRoute() {
+      return `/user/${this.$store.state.user?.id}`;
     },
   },
 });
