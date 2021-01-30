0519b0b28526754a51aa6e6f1a2ca376bbbada7b
Albert Gergo <gergoalbert.gta@gmail.com>
2021-01-30 12:48:33 +0200

    refactor: posts are in the global store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,3 +1,4 @@
+import { Post } from "@/types/post";
 import Vue from "vue";
 import Vuex from "vuex";
 
@@ -6,10 +7,17 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     user: null,
+    posts: [] as Post[],
   },
   mutations: {
     addUser: (state, user) => {
       state.user = user;
+    },
+    addPost: (state, post) => {
+      state.posts = [post, ...state.posts];
+    },
+    setPosts: (state, posts) => {
+      state.posts = posts;
     },
   },
   actions: {},
--- client/src/types/post.ts
+++ client/src/types/post.ts
@@ -1,5 +1,9 @@
+import { User } from "./user";
+
 export interface Post {
   id: number;
   descripition: string;
   image: string;
+  liked: boolean;
+  user: User;
 }
