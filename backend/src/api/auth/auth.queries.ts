3dceb69f4383099d6412f007716283a97654a2bf
Albert Gergo <gergoalbert.gta@gmail.com>
2021-02-05 14:54:51 +0200

    drop: deleted legacy code

--- backend/src/api/auth/auth.queries.ts
+++
@@ -1,8 +0,0 @@
-import User from "./auth.model";
-
-export async function isEmailUnique(email: string) {
-  const findEmail = await User.query().findOne({
-    email,
-  });
-  return !Boolean(findEmail);
-}
